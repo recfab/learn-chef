0fa771394eaee71f0e8f167073b146e2e3006f24
Yael Namen <aaron.bdd@gmail.com>
2017-01-02 20:47:02 -0800

    step

--- 01_learn_the_basics/cookbooks/learn_chef_apache2/recipes/default.rb
+++ 01_learn_the_basics/cookbooks/learn_chef_apache2/recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+apt_update 'Update the apt cache daily' do
+    frequency 86_400
+    action :periodic
+end
+
+package 'apache2'
+
+service 'apache2' do
+    supports :status => true
+    action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
